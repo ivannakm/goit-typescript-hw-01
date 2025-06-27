705b34c0e5732e2f8109355af1e8340a5d1109c3
ivannakm <ivadatalover@gmail.com>
2025-06-27 12:15:53 +0200

    add ts

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+// Створіть змінну, яка може містити або рядок, або число(union type) ?
+// Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable'(literal type).
+
+let mixedType: string | number;
+type YesOrNo = "enable" | "disable";
+let value: YesOrNo;
+value = "enable";
+value = "disable";
